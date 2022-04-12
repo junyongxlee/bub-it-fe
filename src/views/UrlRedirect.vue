e2fbd9f24a404d7bd3b69524f77cbdbef7bec5d1
Lee Jun Yong <junyongxlee@gmail.com>
2022-04-13 04:24:23 +0800

    Redirection done

--- src/views/UrlRedirect.vue
+++ src/views/UrlRedirect.vue
@@ -3,8 +3,35 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "UrlRedirect",
   components: {},
+  mounted() {
+    this.getUrlThenRedirect();
+  },
+  methods: {
+    getUrlThenRedirect: function () {
+      axios({
+        method: "GET",
+        url: "url",
+        params: { alias: this.$route.params.alias },
+      }).then(
+        (result) => {
+          window.location.href = this.addhttp(result.data.url.destination_url);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    },
+    addhttp: function (url) {
+      if (!/^(?:f|ht)tps?:\/\//.test(url)) {
+        url = "http://" + url;
+      }
+      console.log(url);
+      return url;
+    },
+  },
 };
 </script>
