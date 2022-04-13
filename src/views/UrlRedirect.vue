b0c4da0afcebbcd38dd6ae790d24851da48fd2d9
Lee Jun Yong <junyongxlee@gmail.com>
2022-04-14 03:52:40 +0800

    Redirecting page done

--- src/views/UrlRedirect.vue
+++ src/views/UrlRedirect.vue
@@ -1,5 +1,38 @@
 <template>
-  <div class="redirect">Redirect</div>
+  <div class="redirect">
+    <div class="background"></div>
+    <div
+      class="
+        d-flex
+        flex-column
+        align-items-center
+        justify-content-center
+        text-center
+      "
+    >
+      <div v-if="error">
+        <img
+          class="mt-5"
+          style="max-width: 250px"
+          src="../assets/images/page-not-found.svg"
+          alt=""
+        />
+        <div class="mt-3 page-not-found">PAGE NOT FOUND</div>
+        <p class="mt-4">
+          We can’t seem to find the page you’re looking for. <br />
+          The link you followed may be broken or you may have entered the wrong
+          link.
+        </p>
+      </div>
+      <img
+        v-else
+        class="mt-5"
+        style="max-width: 350px"
+        src="../assets/images/redirecting.png"
+        alt=""
+      />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -10,6 +43,11 @@ export default {
   mounted() {
     this.getUrlThenRedirect();
   },
+  data() {
+    return {
+      error: false,
+    };
+  },
   methods: {
     getUrlThenRedirect: function () {
       axios({
@@ -18,10 +56,12 @@ export default {
         params: { alias: this.$route.params.alias },
       }).then(
         (result) => {
+          console.log(result);
           window.location.href = this.addhttp(result.data.url.destination_url);
         },
         (error) => {
           console.log(error);
+          this.error = true;
         }
       );
     },
@@ -35,3 +75,30 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.redirect {
+  .background {
+    background-size: 100% 100% !important;
+    height: 100%;
+    width: 100%;
+    z-index: -1;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    background: linear-gradient(142.99deg, #f5f5f3 21.49%, #faf3ec 91.63%);
+  }
+}
+
+.page-not-found {
+  font-weight: 600;
+  font-size: 35px;
+  color: #000000;
+}
+
+p {
+  font-weight: 400 !important;
+  font-size: 17px !important;
+  color: #868686 !important;
+}
+</style>
